b28cf94321708f3576ff0a2bc3d9f871af6ada03
Maahi Joshi <149944558+Maahijoshi28@users.noreply.github.com>
2026-03-03 22:06:54 +0530

    Delete src directory

--- src/agents/__init_.py
+++
@@ -1,6 +0,0 @@
-# src/agents/specialized/__init__.py
-from .researcher import researcher_agent
-from .summarizer import summarizer_agent
-
-# This tells Python which functions are 'public' in this sub-package
-__all__ = ["researcher_agent", "summarizer_agent"]
--- src/tools/delegation.py
+++
@@ -1,8 +0,0 @@
-from agents.specialized.researcher import researcher_agent
-from agents.specialized.summarizer import summarizer_agent
-
-def task(type, content):
-    if type == "research":
-        return researcher_agent(content)
-    else:
-        return summarizer_agent(content)
